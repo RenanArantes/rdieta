866227b5e7e8d29ae0eeba25416792bf4c9d4b36
RenanArantes <malevo18@gmail.com>
2022-11-09 10:18:44 -0300

    icons and pwa config without building

---
+++ @types/completeFood.d.ts
@@ -0,0 +1,69 @@
+export default interface CompleteFood {
+  category: string
+  humidity_percents: number
+  energy_kcal: number
+  energy_kj: number
+  protein_g: number
+  lipid_g: number
+  cholesterol_mg: number | string
+  carbohydrate_g: number
+  fiber_g: number
+  ashes_g: number
+  calcium_mg: number
+  magnesium_mg: number
+  manganese_mg: number
+  phosphorus_mg: number
+  iron_mg: number
+  sodium_mg: number
+  potassium_mg: number
+  copper_mg: number
+  zinc_mg: number
+  retinol_mcg: number | string
+  re_mcg: number | string
+  rae_mcg: number | string
+  thiamine_mg: number | string
+  riboflavin_mg: number | string
+  pyridoxine_mg: number | string
+  niacin_mg: number | string
+  vitaminC_mg: number | string
+  saturated_g: number | string
+  monounsaturated_g: number | string
+  polyunsaturated_g: number | string
+  '12:0_g': number | string
+  '14:0_g': number | string
+  '16:0_g': number | string
+  '18:0_g': number | string
+  '20:0_g': number | string
+  '22:0_g': number | string
+  '24:0_g': number | string
+  '14:1_g': number | string
+  '16:1_g': number | string
+  '18:1_g': number | string
+  '20:1_g': number | string
+  '18:2 n-6_g': number | string
+  '18:3 n-3_g': number | string
+  '20:4_g': number | string
+  '20:5_g': number | string
+  '22:5_g': number | string
+  '22:6_g': number | string
+  '18:1t_g': number | string
+  '18:2t_g': number | string
+  tryptophan_g: number | string
+  threonine_g: number | string
+  isoleucine_g: number | string
+  leucine_g: number | string
+  lysine_g: number | string
+  methionine_g: number | string
+  cystine_g: number | string
+  phenylalanine_g: number | string
+  tyrosine_g: number | string
+  valine_g: number | string
+  arginine_g: number | string
+  histidine_g: number | string
+  alanine_g: number | string
+  aspartic_g: number | string
+  glutamic_g: number | string
+  glycine_g: number | string
+  proline_g: number | string
+  serine_g: number | string
+}
--- @types/food.d.ts
+++ @types/food.d.ts
@@ -1,4 +1,6 @@
-export default interface Food {
+import CompleteFood from './completeFood'
+
+export default interface Food extends CompleteFood {
   id: number
   description: string
   energy_kcal: number
