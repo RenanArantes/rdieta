ed4f2dbb8273dd690dbe0a4e180bee8689b3efbb
RenanArantes <malevo18@gmail.com>
2022-12-06 09:59:56 -0300

    correction: crossOrigin anonymous

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,7 +8,11 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="anonymous"
+          />
           <link
             href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;700&display=swap"
             rel="stylesheet"
