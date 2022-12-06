210d3d57e0fb42cfa75d03e70c8a21088c620bb0
RenanArantes <malevo18@gmail.com>
2022-12-06 09:29:21 -0300

    theme config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,27 +1,31 @@
 import type { AppProps } from 'next/app'
+import { ThemeProvider } from 'styled-components'
 import { Header } from '../components/Header'
 import { DietContextProvider } from '../contexts/Diet'
 import { MealContextProvider } from '../contexts/Meal'
 import { PersonContextProvider } from '../contexts/Person'
 import { StepContextProvider } from '../contexts/Step'
+import { defaultTheme } from '../styles/themes/default'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <StepContextProvider>
-      <PersonContextProvider>
-        <DietContextProvider>
-          <MealContextProvider>
-            <div
-              style={{
-                display: 'block',
-              }}
-            >
-              <Header />
-              <Component {...pageProps} />
-            </div>
-          </MealContextProvider>
-        </DietContextProvider>
-      </PersonContextProvider>
-    </StepContextProvider>
+    <ThemeProvider theme={defaultTheme}>
+      <StepContextProvider>
+        <PersonContextProvider>
+          <DietContextProvider>
+            <MealContextProvider>
+              <div
+                style={{
+                  display: 'block',
+                }}
+              >
+                <Header />
+                <Component {...pageProps} />
+              </div>
+            </MealContextProvider>
+          </DietContextProvider>
+        </PersonContextProvider>
+      </StepContextProvider>
+    </ThemeProvider>
   )
 }
---
+++ styles/themes/default.ts
@@ -0,0 +1,18 @@
+export const defaultTheme = {
+  blue: {
+    900: '#17212a',
+    700: '#32495d',
+    400: '#4e7190',
+    300: '#6a99c3',
+    200: '#85c1f6',
+  },
+  white: '#fff',
+  black: '#000',
+  alert: {
+    error: '#d9042b',
+    block: '#730220',
+    info: '#038c7c',
+    success: '#4ad948',
+    warning: '#f29f05',
+  },
+}
