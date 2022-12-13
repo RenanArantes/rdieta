d178f2c27ad30af1d7590167a3d084d4109e8bc7
RenanArantes <malevo18@gmail.com>
2022-12-13 14:58:49 -0300

    remove button to /diet

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,15 @@
 import { useContext } from 'react'
-import { useRouter } from 'next/router'
 import { PersonContext } from '../contexts/Person'
 import { BMRForm } from '../components/forms/BMRForm'
 import { TotalKcalForm } from '../components/forms/TotalKcalForm'
 import { StepContext } from '../contexts/Step'
 import { BottomMultiStep } from '../components/BottomMultiStep'
-import { Button } from '../components/Button'
 
 export default function Home() {
   const {
     personData: { bmr, totalCaloricSpending },
   } = useContext(PersonContext)
   const { step, currentStep } = useContext(StepContext)
-
-  const { push } = useRouter()
 
   return (
     <div>
@@ -36,9 +32,6 @@ export default function Home() {
           )}
         </div>
       </div>
-      <Button type="button" onClick={() => push('/diet')}>
-        Ir para Dieta
-      </Button>
       <BottomMultiStep step={step} currentStep={currentStep} />
     </div>
   )
