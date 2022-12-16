c16b34d5406b83b5dfd58c1d86855a8da1a3223d
RenanArantes <malevo18@gmail.com>
2022-12-16 15:09:37 -0300

    removed: dietAccordion style

--- styles/pages/meal.ts
+++ styles/pages/meal.ts
@@ -4,13 +4,7 @@ export const DietContainer = styled.div`
   display: flex;
   flex-direction: column;
 `
-interface DietAccordionProps {
-  show: boolean
-}
 
-export const DietAccordion = styled.div<DietAccordionProps>`
-  display: ${(props) => (props.show ? 'block' : 'none')};
-`
 export const DataContainer = styled.div`
   display: flex;
   justify-content: space-between;
